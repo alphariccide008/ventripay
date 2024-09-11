4422146861b8882f4b9a4bd0a521a2a683f448a0
Nelson Braimoh <braimohnelson@gmail.com>
2024-09-11 18:29:22 +0100

    initial commit

--- ventripay-app/src/components/Footer.jsx
+++ ventripay-app/src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { footerLinks, socialMedia } from "../data";
 
 const Footer = () => {
   return (
-    <footer className="max-container bg-[#003366] px-12 py-6 ">
-      <div className="flex max-sm:gap-10 max-md:gap-5 flex-wrap justify-between max-sm:text-center max-lg:flex-col items-start">
-        <div className="flex items-center">
-          <img src={footerIcon} alt="logo" width={100} height={100} />
-          <div>
+<footer className="max-container bg-[#003366] px-12 py-6 divide-y divide-zinc-500 mb-3">
+<div className="flex flex-wrap items-start max-sm:gap-10 max-md:gap-5 max-lg:flex-col ">
+        <div className="flex items-center max-sm:mx-auto mt-6 xl:ml-6">
+          <img src={footerIcon} alt="logo" width={50} height={50} />
+          <div className="flex flex-col ml-3">
             <p className="text-white mt-4">ventripay</p>
-            <p className="text-white leading-4 mt-2">
+            <p className="text-white leading-4 mt-2 ">
               shaping the future of crossborder <br />
               payment in africa.
             </p>
@@ -18,14 +18,14 @@ const Footer = () => {
         </div>
         
 
-        <div className="flex max-sm:flex-col max-sm:gap-10 mx-auto lg:gap-40  gap-20 flex-wrap  mt-8">
+        <div className="flex max-sm:flex-col max-sm:text-center max-sm:gap-10 mx-auto lg:gap-40 gap-20 flex-wrap mt-8">
           {footerLinks.map((footer) => {
             return (
               <div key={footer.title}>
                 <h4 className="text-[#FF6600] font-semibold mb-5 uppercase">{footer.title}</h4>
                 <ul>
                   {footer.links.map((link) => (
-                    <li key={link.name} className="text-white">
+                    <li key={link.name} className="text-white mb-4">
                       <a href="#">{link.name}</a>
                     </li>
                   ))}
@@ -35,7 +35,7 @@ const Footer = () => {
           })}
         </div>
       </div>
-      <hr className="border-zinc-300 w-full mt-4" />
+      {/* <hr className="border-zinc-300 w-full mt-4" /> */}
 
       <div className="flex flex-col xl:flex-row justify-between items-center ">
         <div className="flex">
--- ventripay-app/src/components/NavBar.jsx
+++ ventripay-app/src/components/NavBar.jsx
@@ -4,19 +4,19 @@ import { logo } from "../assets/icons";
 
 const NavBar = () => {
   return (
-    <header className=" w-full py-5  sm:px-16 px-8">
-      <nav className="flex justify-between max-container items-center  ">
+    <header className=" max-w-full py-5 sm:px-16  px-8">
+      <nav className="w-full flex justify-between max-container items-center max-sm:h-12 max-md:h-14 h-24 ">
         <a href="/" className="text-[30px]">
          <img src={logo} 
-         width={100}
-         height={100}
+         width={70}
+         height={70}
          alt="logo" />
         </a>
 
         <ul className="flex flex-1 gap-16 justify-center max-xl:hidden">
           {navLinks.map((item) => (
             <li key={item.name}>
-              <a href="#" className="text-lg uppercase">
+              <a href="#" className="text-lg uppercase font-semibold">
                 {item.name}
               </a>
             </li>
@@ -24,7 +24,7 @@ const NavBar = () => {
         </ul>
 
         <div className="flex gap-10 max-xl:hidden text-lg ">
-          <a href="#" className=" flex items-center py-3 px-12 text-blue border-2 border-[#003366] rounded-lg uppercase">login</a>
+          <a href="#" className="font-semibold flex items-center py-3 px-12 text-blue border-2 border-[#003366] rounded-lg uppercase">login</a>
           <a href="#" className="flex items-center py-3 px-12 bg-[#003366] rounded-lg text-white uppercase">register</a>
         </div>
         <div className=" hidden max-xl:block ">
